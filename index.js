efd5b7c2e0693e22c310745f99810b4dbc6b55b7
Fifee <fifee2000@gmail.com>
2023-04-11 14:54:06 +0800

    login

--- index.js
+++ index.js
@@ -1,30 +1,78 @@
+let dbUsers = [
+  {
+      username: "Fifee",
+      password: "abcd",
+      name: "Fifee Zaheed",
+      email: "fifee2000@gmail.com"
+  },
+  {
+      username: "Kei",
+      password: "1234",
+      name: "Keima",
+      email: "kei@gmail.com"
+  },
+  {
+      username: "Kei",
+      password: "1234",
+      name: "Keima",
+      email: "kei@gmail.com"  
+  }
+
+]
+
+function login(username, password) {
+  //let matchUser= dbUsers.find(user => user.username == username)
+  let matchUser= dbUsers.find(x => x.username == username)
+  if(!matchUser) return "User not found!"
+  if(matchUser.password==password){
+      return matchUser
+  } else {
+      return "Invalid password"
+  }
+} 
+
+function register(username, password, name, email) {
+  dbUsers.push({
+      username: username,
+      password: password,
+      name: name,
+      email: email
+  })
+}
+
 const express = require('express')
 const app = express()
 const port = 3000
 
+
 app.use(express.json())
 
 app.post('/login', (req, res) => {
-    console.log(req.body)
+  console.log(req.body)
 
-    res.send('login')
+  res.send('Login')
+})
 
-})  
 
 app.get('/', (req, res) => {
-  res.send('Hello UTeM!')
+  res.send('Hi World!')
 })
 
 app.get('/bye', (req, res) => {
-  res.send('Bye bye UTeM!')
+  res.send('bye World!')
 })
 
 app.post('/register', (req, res) => {
+  let result=register(
+    req.body.username,
+    req.body.password,
+    req.body.name,
+    req.body.email
+  )
+  
   res.send('ACCOUNT CREATED!')
-})  
-
-
+})
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log('Example app listening on port ${port}')
 })
